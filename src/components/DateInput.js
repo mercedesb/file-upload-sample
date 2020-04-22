b033c9b995b0f6824d85f94843b85225a4bb0dd2
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-04-22 10:45:27 -0500

    create DateInput component

---
+++ src/components/DateInput.js
@@ -0,0 +1,36 @@
+import React from "react";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import { formatDate, parseDate } from "react-day-picker/moment";
+import "react-day-picker/lib/style.css";
+import PropTypes from "prop-types";
+
+export function DateInput({ name, value, onChange }) {
+  const DATE_FORMAT = "MM-DD-YYYY";
+
+  const handleChange = (day) => {
+    onChange(formatDate(day, DATE_FORMAT));
+  };
+
+  return (
+    <div className="input">
+      <label htmlFor={name}>{name}</label>
+
+      <DayPickerInput
+        name={name}
+        onDayChange={handleChange}
+        value={!!value ? parseDate(value) : undefined}
+        selectedDay={!!value ? parseDate(value) : undefined}
+        formatDate={formatDate}
+        parseDate={parseDate}
+        format={DATE_FORMAT}
+        placeholder={`${formatDate(new Date(), DATE_FORMAT)}`}
+      />
+    </div>
+  );
+}
+
+DateInput.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
---
+++ src/components/__tests__/DateInput.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { formatDate, parseDate } from "react-day-picker/moment";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import { DateInput } from "../DateInput";
+
+let subject;
+let name;
+let value;
+let onChange;
+
+describe("DateInput", () => {
+  beforeEach(() => {
+    name = "test";
+    value = "04-22-2020";
+    onChange = jest.fn();
+    subject = shallow(
+      <DateInput name={name} value={value} onChange={onChange} />
+    );
+  });
+
+  describe("render", () => {
+    it("renders correctly", () => {
+      const label = subject.find("label");
+      expect(label).toHaveLength(1);
+      expect(label.prop("htmlFor")).toEqual(name);
+
+      const dayPicker = subject.find(DayPickerInput);
+      expect(dayPicker).toHaveLength(1);
+      expect(dayPicker.prop("name")).toEqual(name);
+      expect(dayPicker.prop("onDayChange")).toEqual(expect.any(Function));
+    });
+  });
+
+  describe("onChange", () => {
+    let newValue;
+
+    beforeEach(() => {
+      name = "test";
+      value = "04-22-2020";
+      onChange = jest.fn();
+      subject = mount(
+        <DateInput name={name} value={value} onChange={onChange} />
+      );
+
+      newValue = "03-07-2020";
+      const dayPicker = subject.find(DayPickerInput).first();
+      const input = dayPicker.find("input").first();
+      input.simulate("change", { target: { value: newValue } });
+    });
+
+    it("formats the date", () => {
+      expect(onChange).toHaveBeenCalledWith(newValue);
+    });
+  });
+});
