a3cf142e0cdd43edbd891bce03cba9acd8db9417
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-04-22 10:30:05 -0500

    update file input to use ref since its an uncontrolled component

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export function FileInput({ name }) {
+export function FileInput({ name, inputRef }) {
   return (
     <div className="input">
       <label htmlFor={name}>{name}</label>
-      <input name={name} id={name} type="file" />
+      <input name={name} id={name} type="file" ref={inputRef} />
     </div>
   );
 }
